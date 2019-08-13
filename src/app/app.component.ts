ede80af6f2c5d8789565a1bf19e4d2afa67e9d92
gpdhanush <agprakash406@gmail.com>
2019-08-13 11:59:36 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { ChartData } from './data';
 @Component({
   selector: 'my-app',
